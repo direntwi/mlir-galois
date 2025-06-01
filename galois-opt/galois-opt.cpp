e4a228aadf6d49dfd964b3cf995564a4bd331851
Yoocee Ansah <yooceeansah@gmail.com>
2025-06-01 14:49:16 +0000

    Register all dialects and extensions

--- galois-opt/galois-opt.cpp
+++ galois-opt/galois-opt.cpp
@@ -9,6 +9,7 @@
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
+#include "mlir/InitAllExtensions.h"
 #include "mlir/Support/FileUtilities.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
@@ -26,7 +27,8 @@ int main(int argc, char **argv) {
   // Add the following to include *all* MLIR Core dialects, or selectively
   // include what you need like above. You only need to register dialects that
   // will be *parsed* by the tool, not the one generated
-  // registerAllDialects(registry);
+  registerAllDialects(registry);
+  registerAllExtensions(registry);                   
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Galois optimizer driver\n", registry));
